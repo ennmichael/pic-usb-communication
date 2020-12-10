4e55148c716a3bb7bcc434fa4eafb40aa33e4129
Enn Michael <enntheprogrammer@gmail.com>
2020-12-10 23:02:53 +0100

    Transmission after reception fails, all other combinations OK

--- host/src/main.rs
+++ host/src/main.rs
@@ -7,6 +7,12 @@ use libusb::Context;
 fn main() {
     let mut ctx = Context::new().unwrap();
     let mut device = RawDevice::acquire(&mut ctx).unwrap();
+    println!("{:?}", device.read_message(5).unwrap());
+    println!("{:?}", device.read_message(5).unwrap());
     device.write_message(&[0x09, 0x02, 0x03]).unwrap();
-    // println!("{:?}", device.read_message(5).unwrap());
+    println!("{:?}", device.read_message(5).unwrap());
+    device
+        .write_message(&[0x69, 0x69, 0x69, 0x69, 0x69, 0x69, 0x69])
+        .unwrap();
+    println!("{:?}", device.read_message(5).unwrap());
 }
